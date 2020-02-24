08a17984c660404b90d2ad8e2b484452a7cce003
AshithaJoseph <42240412+AshithaJoseph@users.noreply.github.com>
2020-02-24 17:03:34 +1100

    Committing Models and Controllers

---
+++ SQLQuery1.sql
@@ -0,0 +1,28 @@
+﻿
+create table Customers (
+	Id INT identity(1,1) PRIMARY KEY NOT NULL,
+	First_name VARCHAR(50) NOT NULL,
+	Last_name VARCHAR(50) NOT NULL,
+	Gender VARCHAR(50) NOT NULL,
+	Dob DATE NOT NULL,
+	Passport VARCHAR(50) NOT NULL,
+	UserId nvarchar(max) NOT NULL
+);
+create table Bookings (
+	Id int identity(1,1) primary key,
+	Customer_id INT FOREIGN KEY references customers(id),
+	Booking_no VARCHAR(50),
+	Journey_date DATE,
+	Origin VARCHAR(50),
+	Destination VARCHAR(50)
+);
+
+create table Flights (
+	Id INT identity(1,1) primary key,
+	Flight_no VARCHAR(50),
+	Travel_date DATE,
+	Origin VARCHAR(50),
+	Destination VARCHAR(50),
+	Depart_time VARCHAR(50),
+	Arrival_time VARCHAR(50)
+);
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Mvc;
+
+[assembly: OwinStartupAttribute(typeof(TheSkyLink.Startup))]
+namespace TheSkyLink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+    }
+}
